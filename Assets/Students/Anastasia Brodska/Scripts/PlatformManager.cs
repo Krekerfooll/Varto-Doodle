1341ea474e633b64a604b4ca504bff0d9632496b
MerlMorison <merlmorison@gmail.com>
2024-04-09 00:08:11 +0300

    deleting platforms

--- Assets/Students/Anastasia Brodska/Scripts/PlatformManager.cs
+++ Assets/Students/Anastasia Brodska/Scripts/PlatformManager.cs
@@ -1,21 +1,39 @@
 using Doodle.Core;
 using UnityEngine;
 
-
 public class PlatformManager : MonoBehaviour
 {
     [SerializeField] private GameObject newPlatformPrefab;
-    [SerializeField] private PlayerMovement playerMovement; 
-   
-
+    [SerializeField] private PlayerMovement playerMovement;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        
         if (collision.gameObject.CompareTag("Platform") && playerMovement != null && playerMovement.IsGrounded)
         {
-            Instantiate(newPlatformPrefab, collision.transform.position, collision.transform.rotation);
+            GameObject newPlatform = Instantiate(newPlatformPrefab, collision.transform.position, collision.transform.rotation);
             Destroy(collision.gameObject);
+            newPlatform.AddComponent<PlatformVisibilityHandler>();
         }
+    }
+}
+
+public class PlatformVisibilityHandler : MonoBehaviour
+{
+    private void Update()
+    {
+        if (!IsVisible())
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private bool IsVisible()
+    {
+        Camera mainCamera = Camera.main;
+        if (mainCamera == null)
+            return false;
+
+        Plane[] planes = GeometryUtility.CalculateFrustumPlanes(mainCamera);
+        return GeometryUtility.TestPlanesAABB(planes, GetComponent<Renderer>().bounds);
     }
 }
