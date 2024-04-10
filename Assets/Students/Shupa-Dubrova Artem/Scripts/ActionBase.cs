9745f712baa1f2048cf419c0e9645554107e55ee
Artem Shupa-Dubrova <fxtema@gmail.com>
2024-04-11 01:02:58 +0300

    Action Base Object spawner
    New commit test

---
+++ Assets/Students/Shupa-Dubrova Artem/Scripts/ActionBase.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+namespace Students.Shupa_Dubrova_Artem.Scripts
+{
+    public abstract class ActionBase : MonoBehaviour
+    {
+        [SerializeField] protected bool _executeOnAwake;
+        [SerializeField] protected bool _executeOnlyOnes;
+
+        protected bool _isExecutedOnes;
+
+        private void Awake()
+        {
+            if (_executeOnAwake)
+            {
+                _isExecutedOnes = true;
+                ExecuteInternal();
+            }
+        }
+
+        protected void Execute()
+        {
+            if (_executeOnlyOnes && _isExecutedOnes)
+                return;
+
+            _isExecutedOnes = true;
+            ExecuteInternal();
+        }
+        
+        protected abstract void ExecuteInternal();
+    }
+    
+}
---
+++ Assets/Students/Shupa-Dubrova Artem/Scripts/ObjectSpawner.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace Students.Shupa_Dubrova_Artem.Scripts
+{
+    public abstract class ObjectSpawner : ActionBase
+    {
+        [SerializeField] private Transform _spawnPoint;
+        [SerializeField] private GameObject[] _prefabsVariants;
+
+        protected override void ExecuteInternal()
+        {
+            var randomObject = _prefabsVariants[Random.Range(0, _prefabsVariants.Length)];
+            Instantiate(randomObject, _spawnPoint);
+        }
+    }
+}
