9972afaf468513288ce9e3ff066fbbfa8dbdbe51
Artur Pashchenko <144344189+Chelentanick@users.noreply.github.com>
2024-04-13 18:04:17 +0200

    Add a platform generator

---
+++ Assets/Students/Pashchenko Arthur/Scripts/SimplePlatformGenerator.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Artur.Pashchenko.Platform
+{
+    public class SimplePlatformGenerator : MonoBehaviour
+    {
+        [Header("Main Settings")]
+        [Space]
+        [SerializeField] private Transform _target;
+        [Space]
+        [Header("Spawn Settings")]
+        [Space]
+        [SerializeField] private Platform _platformPrefab;
+        [SerializeField] private int _stepsToSpawn;
+        [SerializeField] private float _stepsToDelete;
+        [SerializeField] private float _stepHeight;
+        [SerializeField] private Vector2 _border;
+
+        private Queue<Platform> _spawnedPlatforms = new Queue<Platform>();
+
+        private float _lastPlatformsSpawnedOnPlayerPos;
+        private float _lastPlatformsDeletedOnPlayerPos;
+
+
+        private void Awake()
+        {
+            _lastPlatformsSpawnedOnPlayerPos = _target.position.y;
+            _lastPlatformsDeletedOnPlayerPos = _target.position.y;
+
+
+
+            for (int i = 0; i < _stepsToSpawn; i++)
+            {
+                SpawnPlatform(i);
+            }
+        }
+        private void Update()
+        {
+            if (_target.position.y - _lastPlatformsSpawnedOnPlayerPos > _stepHeight)
+            {
+                SpawnPlatform(_stepsToSpawn);
+                _lastPlatformsSpawnedOnPlayerPos += _stepHeight;
+            }
+
+            if (_target.position.y - _lastPlatformsSpawnedOnPlayerPos > _stepHeight)
+            {
+                var platformToDelete = _spawnedPlatforms.Dequeue();
+
+                if (platformToDelete && platformToDelete.gameObject)
+                {
+                    Destroy(platformToDelete.gameObject);
+                }
+                _lastPlatformsDeletedOnPlayerPos += _stepHeight;
+
+            }
+
+
+        }
+            private void SpawnPlatform(int stepsCount)
+            {
+                var X = Random.Range(_border.x, _border.y);
+                var Y = _target.position.y + stepsCount * _stepHeight;
+
+                var platformPosition = new Vector3(X, Y, transform.position.z);
+                var spawnedPlatform = Instantiate(_platformPrefab, platformPosition, Quaternion.identity, transform);
+
+                spawnedPlatform.Init(_target);
+
+                _spawnedPlatforms.Enqueue(spawnedPlatform);
+            }
+        }
+    } 
