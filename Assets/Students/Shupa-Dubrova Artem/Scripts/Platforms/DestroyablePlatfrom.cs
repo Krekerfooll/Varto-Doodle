5410c6b0fc50e2509147d655b2953ba121e43034
Artem Shupa-Dubrova <fxtema@gmail.com>
2024-04-16 02:09:26 +0300

    didnt rewrite but added Destroyable platform with animations

--- Assets/Students/Shupa-Dubrova Artem/Scripts/Platforms/DestroyablePlatfrom.cs
+++ Assets/Students/Shupa-Dubrova Artem/Scripts/Platforms/DestroyablePlatfrom.cs
@@ -5,9 +5,23 @@ namespace Students.Shupa_Dubrova_Artem.Scripts.Platforms
 {
     public class DestroyablePlatfrom : OnCollisionEvents
     {
+        [SerializeField] private GameObject _gameObject;
+        [SerializeField] private Animator _animator;
+        [SerializeField] private float _delay;
+        
         protected override void ExecuteInternal()
         {
-            throw new System.NotImplementedException();
+            DestroyPlatform();
         }
+
+        private void DestroyPlatform()
+        {
+            Destroy(_gameObject, _delay);
+            if (_animator is not null)
+            {
+                _animator.SetBool("isDestroyed", true);
+            }
+        }
+        
     }
 }
