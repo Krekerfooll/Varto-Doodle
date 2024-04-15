04d3f601fb88abce33b2350feed16148022624ef
MerlMorison <merlmorison@gmail.com>
2024-04-15 18:40:34 +0300

    Platforms dest

---
+++ Assets/Students/Anastasia Brodska/Scripts/NewScripts/Destroyer.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class Destroyer : PlatformOnColision
+{
+    public enum DestructionType { Self, TargetObject, CollidedObject }
+
+    [SerializeField] private DestructionType _destructionType;
+    [SerializeField] private GameObject _objectToDestroy;
+    [SerializeField] private float _delay;
+
+    public void Destroy()
+    {
+        switch (_destructionType)
+        {
+            case DestructionType.Self:
+                Destroy(gameObject, _delay);
+                break;
+
+            case DestructionType.TargetObject:
+                Destroy(_objectToDestroy, _delay);
+                break;
+
+            case DestructionType.CollidedObject:
+                Destroy(LastCollision.gameObject, _delay);
+                break;
+        }
+    }
+
+    protected override void ExecuteInternal()
+    {
+        Destroy();
+    }
+}
---
+++ Assets/Students/Anastasia Brodska/Scripts/NewScripts/PlayerMovement.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+namespace Doodle.Core
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        Animator anim;
+        [SerializeField] Rigidbody2D _rigidbody;
+        [SerializeField] private float _jumpPower;
+        [SerializeField] private float _moveSpeed;
+        [SerializeField] private float _castLength;
+        [SerializeField] private LayerMask _groundMask;
+        private bool _isGrounded;
+        public bool IsGrounded => _isGrounded;
+
+        void Awake()
+        {
+            anim = GetComponent<Animator>();
+        }
+
+        private void FixedUpdate()
+        {
+            _isGrounded = Physics2D.Raycast(transform.position, Vector2.down, _castLength, _groundMask);
+            Debug.DrawLine(_rigidbody.transform.position, _rigidbody.transform.position + Vector3.down * _castLength, Color.green);
+
+            Run();
+            if (_isGrounded)
+            {
+                Jump();
+            }
+
+        }
+        void Run()
+        {
+            Vector3 localScale = transform.localScale;
+
+            var direction = Input.GetAxis("Horizontal");
+
+            _rigidbody.velocity = new Vector2(direction * _moveSpeed, _rigidbody.velocity.y);
+
+            if (direction < 0)
+            {
+                localScale.x = Mathf.Abs(localScale.x) * -1;
+                transform.localScale = localScale;
+                anim.SetBool("isRun", true);
+            }
+            else if (direction > 0)
+            {
+                localScale.x = Mathf.Abs(localScale.x);
+                transform.localScale = localScale;
+                anim.SetBool("isRun", true);
+            }
+            else
+            {
+                anim.SetBool("isRun", false);
+            }
+
+        }
+
+        void Jump()
+        {
+            if ((Input.GetButtonDown("Jump") || Input.GetAxisRaw("Vertical") > 0) && !anim.GetBool("isJump"))
+            {
+                anim.SetBool("isJump", true);
+                _rigidbody.AddForce(Vector2.up * _jumpPower, ForceMode2D.Impulse);
+            }
+            else if (_isGrounded && anim.GetBool("isJump"))
+            {
+                anim.SetBool("isJump", false);
+            }
+        }
+    }
+}
