0340c6e0284a12c4b2c0e0ce9c47729d67967810
Volodymyr Dr <82114723+MARSDOC@users.noreply.github.com>
2024-04-18 13:48:52 +0300

    + Pause Menu UI

---
+++ Assets/Students/Drobiniak Volodymyr/Scripts/UI/PauseMenu/PauseMenu.cs
@@ -0,0 +1,50 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Students.Drobiniak_Volodymyr.Scripts.UI.PauseMenu
+{
+    public class PauseMenu : MonoBehaviour
+    {
+        [SerializeField] private bool pauseGame;
+        [SerializeField] private GameObject pauseGameMenu;
+
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (pauseGame)
+                {
+                    ResumeGame();
+                }
+                else
+                {
+                    PauseGame();
+
+                }
+            }
+        }
+
+        public void ResumeGame()
+        {
+            pauseGame = false;
+            pauseGameMenu.SetActive(false);
+            Time.timeScale = 1f;
+        }
+
+        public void PauseGame()
+        {
+            pauseGame = true;
+            pauseGameMenu.SetActive(true);
+            Time.timeScale = 0f;
+        }
+
+        public void LoadMainMenu()
+        {
+            Time.timeScale = 1f;
+            SceneManager.LoadScene("MainMenu");
+        }
+        
+    }
+}
