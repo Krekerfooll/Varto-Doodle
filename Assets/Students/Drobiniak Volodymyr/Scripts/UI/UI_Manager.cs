8730abe1336c78a289e886b61ac062621130d938
Volodymyr Dr <82114723+MARSDOC@users.noreply.github.com>
2024-04-20 15:11:17 +0300

    +Timer

--- Assets/Students/Drobiniak Volodymyr/Scripts/UI/UI_Manager.cs
+++ Assets/Students/Drobiniak Volodymyr/Scripts/UI/UI_Manager.cs
@@ -1,3 +1,4 @@
+using System;
 using Students.Drobiniak_Volodymyr.Scripts.Player;
 using TMPro;
 using UnityEngine;
@@ -5,14 +6,42 @@ using UnityEngine.UI;
 
 namespace Students.Drobiniak_Volodymyr.Scripts.UI
 {
-    public class UI_Manager : MonoBehaviour
+    public class UIManager : MonoBehaviour
     {
+        [Header("Score UI")]
         [SerializeField] private NewPlayerController playerScript;
         [SerializeField] private TextMeshProUGUI scoreText;
+        [Space(2)]
+        
+        
+        [Header("TIMER")]
+        [SerializeField] private float totalTime = 60f; 
+        [SerializeField] private TextMeshProUGUI _timerText; // Посилання на текстовий елемент для відображення таймера
+        private float _currentTime; // Поточний час таймера
+
+        private void Start()
+        {
+            _timerText ??= GetComponent<TextMeshProUGUI>();
+            _currentTime = totalTime;
+        }
+
+
         private void Update()
         {
             string gemCountString = playerScript.gemCounter.ToString();
             scoreText.text = $"GEMS: {gemCountString}";
+            Timer();
+            
+        }
+
+        private void Timer()
+        {
+            // Оновлення таймера
+            _currentTime -= Time.deltaTime;
+            // Форматування тексту для відображення у форматі хвилин:секунди
+            int minutes = Mathf.FloorToInt(_currentTime / 60);
+            int seconds = Mathf.FloorToInt(_currentTime % 60);
+            _timerText.text = $"{minutes:00}:{seconds:00}";
         }
     }
 }
