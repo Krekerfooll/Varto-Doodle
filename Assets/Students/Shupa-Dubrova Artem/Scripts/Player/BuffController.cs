8d96970b6df4f7e9f70cd45eab9ec6fc234dca26
Artem Shupa-Dubrova <fxtema@gmail.com>
2024-04-22 19:32:20 +0300

    added font

---
+++ Assets/Students/Shupa-Dubrova Artem/Scripts/Player/BuffController.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Students.Shupa_Dubrova_Artem.Scripts.Player
+{
+    public class BuffController : MonoBehaviour
+    {
+        
+    }
+}
--- Assets/Students/Shupa-Dubrova Artem/Scripts/Player/RainbowEffect.cs
+++ Assets/Students/Shupa-Dubrova Artem/Scripts/Player/RainbowEffect.cs
@@ -1,5 +1,3 @@
-using System;
-using Students.Shupa_Dubrova_Artem.Scripts.Utils;
 using UnityEngine;
 
 namespace Students.Shupa_Dubrova_Artem.Scripts.Player
@@ -7,19 +5,18 @@ namespace Students.Shupa_Dubrova_Artem.Scripts.Player
     public class RainbowEffect : MonoBehaviour
     {
         [SerializeField] private SpriteRenderer _spriteRenderer;
-        [SerializeField] private Color[] _colors;
+        [SerializeField] private Color _color1;
+        [SerializeField] private Color _color2;
         [SerializeField] private float _speedOfChange;
-
-        private Color _lerpedColor;
 
         private void LateUpdate()
         {
             RainbowBuff();
         }
 
-        protected void RainbowBuff()
+        private void RainbowBuff()
         {
-            _spriteRenderer.color = Color.Lerp(_colors[0], _colors[1], Mathf.PingPong(Time.time * _speedOfChange, 1));
+            _spriteRenderer.color = Color.Lerp(_color1, _color2, Mathf.PingPong(Time.time * _speedOfChange, 1));
         }
     }
 }
