0a574e0ec2731075192952a9e69392a77de6b3f5
Artur Pashchenko <144344189+Chelentanick@users.noreply.github.com>
2024-04-22 23:28:23 +0200

    Aded change of day and night, and change a platform sprite

--- Assets/Students/Pashchenko Arthur/Scripts/ChangeOfDayAndNight.cs
+++ Assets/Students/Pashchenko Arthur/Scripts/ChangeOfDayAndNight.cs
@@ -1,32 +1,53 @@
-
 using UnityEngine;
+
 namespace Artur.Pashchenko.DayAndNight
 {
-
     public class ChangeOfDayAndNight : MonoBehaviour
     {
-        [SerializeField] Color _dayColor;
-        [SerializeField] Color _nightColor;
+        [SerializeField] private Color _dayColor;
+        [SerializeField] private Color _nightColor;
         private Color _targetColor;
         [SerializeField] private float _cycleTime;
         [SerializeField] private float _cycleDistance;
 
         private bool _isDay;
+        private bool _isChangingColor;
         private float _lastCyclePosition;
-
+        private float _colorChangeStartTime;
+        private float _currentPosition;
 
         private void Start()
         {
             _lastCyclePosition = transform.position.y;
         }
-        private void Update() 
+
+        private void Update()
         {
-            if (Mathf.FloorToInt(_lastCyclePosition / _cycleDistance) < Mathf.FloorToInt(transform.position.y / _cycleDistance))
+            _currentPosition = transform.position.y;
+
+            if (!_isChangingColor && _currentPosition - _lastCyclePosition >= _cycleDistance)
             {
+                _isDay = !_isDay;
                 _lastCyclePosition = transform.position.y;
+                _targetColor = _isDay ? _nightColor : _dayColor;
+                _colorChangeStartTime = Time.time;
+                _isChangingColor = true;
+                Debug.Log("Changing Color...");
+            }
+
+            if (_isChangingColor)
+            {
+                float elapsedTime = Time.time - _colorChangeStartTime;
+                float t = Mathf.Clamp01(elapsedTime / _cycleTime);
+                Color startColor = Camera.main.backgroundColor;
+                Camera.main.backgroundColor = Color.Lerp(startColor, _targetColor, t);
+
+                if (t >= 1.0f)
+                {
+                    _isChangingColor = false;
+                    Debug.Log("Color Change Complete");
+                }
             }
         }
-
-
     }
 }
