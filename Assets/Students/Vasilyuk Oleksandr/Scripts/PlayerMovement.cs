f0e702a49cd8d3c76428756ab685b03ff909a24b
SanderStudio <venommax8@gmail.com>
2024-04-22 17:12:21 +0300

    Prac8_RayCast_Doesn'tWork

--- Assets/Students/Vasilyuk Oleksandr/Scripts/PlayerMovement.cs
+++ Assets/Students/Vasilyuk Oleksandr/Scripts/PlayerMovement.cs
@@ -1,30 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class PlayerMovement : MonoBehaviour
 {
-    
-    //public float Speed (get; )
     [SerializeField] Rigidbody2D _rigidbody;
     [SerializeField] private float _jumpPower;
     [SerializeField] private float _moveSpeed;
 
+    [SerializeField] private float _groundCheckDistance;
+    private bool _isGrounded;
+    private Transform _player;
+    //private int _isGroundedMask = 9;
 
-    void Start()
-    {
-        
-    }
+
 
     void Update()
     {
         var direction = Input.GetAxis("Horizontal");
         _rigidbody.velocity = new Vector2(direction*_moveSpeed, _rigidbody.velocity.y);
 
+        _isGrounded = Physics2D.Raycast(_player.position, Vector3.down, _groundCheckDistance);
+        Debug.DrawLine(_player.position, _player.position + Vector3.down * _groundCheckDistance, Color.magenta);
+
+
+
         if (Input.GetKeyDown(KeyCode.W))
         {
             _rigidbody.AddForce(Vector2.up * _jumpPower, ForceMode2D.Impulse);
-
         }
     }
 }
