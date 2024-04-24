02610328d3b080a0707dc11f901eb457e7069f2a
Arthur Ustich <ta.ustich@gmail.com>
2024-04-25 00:24:06 +0300

    PZ_11 Made more adaptive UI

---
+++ Assets/Students/Ustich Arthur/Scripts/UI/UI_Controller.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UI_Controller : MonoBehaviour
+{
+    [SerializeField] private List<TextMeshProUGUI> _textMeshProUGUIs = new List<TextMeshProUGUI>();
+
+    private void Awake()
+    {
+        FontResize();
+    }
+    private void FontResize()
+    {
+        float maxFontSize = _textMeshProUGUIs[0].fontSize;
+        foreach (TextMeshProUGUI tempText in _textMeshProUGUIs)
+        {
+            if(tempText.fontSize > maxFontSize)
+                maxFontSize = tempText.fontSize;
+        }
+
+        for (int i = 0; i < _textMeshProUGUIs.Count; i++)
+        {
+            _textMeshProUGUIs[i].enableAutoSizing = false;
+            _textMeshProUGUIs[i].fontSize = maxFontSize;
+        }
+    }
+}
