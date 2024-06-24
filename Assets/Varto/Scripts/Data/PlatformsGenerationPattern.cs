6b580b8f3d4435eff1f4589c561fdb43a068f222
Ustich Arthur <ta.ustich@gmail.com>
2024-06-25 00:21:55 +0300

    Squashed commit of the following:
    commit a64204f6ff2045f1e42cb63d9f065a29f4e23086
    Author: RomanKapustinskyi <kapustinskyiroman@gmail.com>
    Date:   Sat Jun 15 14:58:26 2024 +0300
    
        Lecture 23
    
    commit 3f0d98eecc8d95fa640fe841b23a2b0637c0ea67
    Author: RomanKapustinskyi <kapustinskyiroman@gmail.com>
    Date:   Wed Jun 12 01:09:52 2024 +0300
    
        Lecture 22

---
+++ Assets/Varto/Scripts/Data/PlatformsGenerationPattern.cs
@@ -0,0 +1,59 @@
+using System.Linq;
+using UnityEngine;
+using static Varto.Examples.Data.PlatformsGroupData;
+
+namespace Varto.Examples.Data
+{
+    [CreateAssetMenu(fileName = "PlatformsGenerationPattern", menuName = "Varto/PlatformsGenerationPattern", order = 0)]
+    public class PlatformsGenerationPattern : ScriptableObject
+    {
+        [SerializeField] private PlatformsGroupSpawnData[] _platformsPattern;
+
+        [System.NonSerialized] private Transform _target;
+        [System.NonSerialized] private Transform _parent;
+        [System.NonSerialized] private Vector2 _boundsX;
+
+        public void Init(Transform target, Transform parent, Vector2 boundsX)
+        {
+            _target = target;
+            _parent = parent;
+            _boundsX = boundsX;
+        }
+
+        public GropSpawnResult SpawnNextGroup(float startSpawnHeight)
+        {
+            var randomGroup = GetRandomGroup();
+
+            return randomGroup.SpawnGroup(_target, _parent, _boundsX, startSpawnHeight);
+        }
+
+        private PlatformsGroupData GetRandomGroup()
+        {
+            var chanceSum = _platformsPattern.Sum(pattern => pattern.Chance);
+            var randomValue = Random.Range(0, chanceSum);
+
+            var currentChance = 0;
+
+            for (int i = 0; i < _platformsPattern.Length; i++)
+            {
+                currentChance += _platformsPattern[i].Chance;
+
+                if (randomValue < currentChance)
+                {
+                    Debug.Log($"Platforms Group with index: {i} was spawned");
+                    return _platformsPattern[i].Group;
+                }
+            }
+
+            Debug.Log($"Platforms Group with index: {_platformsPattern.Length - 1} was spawned");
+            return _platformsPattern.Last().Group;
+        }
+
+        [System.Serializable]
+        public struct PlatformsGroupSpawnData
+        {
+            public PlatformsGroupData Group;
+            public int Chance;
+        }
+    }
+}
---
+++ Assets/Varto/Scripts/Data/PlatformsGroupData.cs
@@ -0,0 +1,66 @@
+using System.Collections.Generic;
+using UnityEngine;
+using Varto.Examples.Platforms;
+
+namespace Varto.Examples.Data
+{
+    [CreateAssetMenu(fileName = "PlatformsGroup", menuName = "Varto/PlatformsGroup", order = 1)]
+    public class PlatformsGroupData : ScriptableObject
+    {
+        [SerializeField] private PlatformRow[] _platformsGroup;
+        [SerializeField] private Vector2 _heightBounds;
+
+        public GropSpawnResult SpawnGroup(Transform target, Transform parent, Vector2 boundsX, float startSpawnHeight)
+        {
+            var spawnedPlatforms = new List<Varto_Platform>();
+            var lastSpawnedHeight = 0f;
+
+            for (int i = 0; i < _platformsGroup.Length; i++)
+            {
+                var row = _platformsGroup[i];
+                lastSpawnedHeight = i == 0 ? startSpawnHeight : lastSpawnedHeight + Random.Range(_heightBounds.x, _heightBounds.y);
+                var spawnedRow = row.SpawnRow(target, parent, boundsX, lastSpawnedHeight);
+
+                spawnedPlatforms.AddRange(spawnedRow);
+            }
+
+            return new GropSpawnResult(spawnedPlatforms, lastSpawnedHeight);
+        }
+
+        public struct GropSpawnResult
+        {
+            public readonly List<Varto_Platform> SpawnedPlatforms;
+            public readonly float LastSpawnedHeight;
+
+            public GropSpawnResult(List<Varto_Platform> spawnedPlatforms, float lastSpawnedHeight)
+            {
+                SpawnedPlatforms = spawnedPlatforms;
+                LastSpawnedHeight = lastSpawnedHeight;
+            }
+        }
+
+        [System.Serializable]
+        public struct PlatformRow
+        {
+            public Varto_Platform[] Platforms;
+
+            public List<Varto_Platform> SpawnRow(Transform target, Transform parent, Vector2 boundsX, float spawnHeight)
+            {
+                var spawnedPlatforms = new List<Varto_Platform>();
+
+                for (int i = 0; i < Platforms.Length; i++)
+                {
+                    var platformPositionX = Random.Range(boundsX.x, boundsX.y);
+                    var platformPosition = new Vector3(platformPositionX, spawnHeight, parent.position.z);
+
+                    var spawnedPlatform = Instantiate(Platforms[i], platformPosition, Quaternion.identity, parent);
+                    spawnedPlatform.Init(target);
+
+                    spawnedPlatforms.Add(spawnedPlatform);
+                }
+
+                return spawnedPlatforms;
+            }
+        }
+    }
+}
